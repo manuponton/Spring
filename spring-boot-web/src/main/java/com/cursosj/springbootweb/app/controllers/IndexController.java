04791fca56eeae8daa9bfd5a5507d672c2238817
Manuel Ponton <manuel.ponton@zabud.com.co>
2021-02-11 15:50:32 -0500

    Se añade ejemplo de iterar una lista con objetos y mostrarlas en una tabla

--- spring-boot-web/src/main/java/com/cursosj/springbootweb/app/controllers/IndexController.java
+++ spring-boot-web/src/main/java/com/cursosj/springbootweb/app/controllers/IndexController.java
@@ -6,20 +6,32 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Arrays;
+
 @RequestMapping("/app")
 @Controller
 public class IndexController {
     //RequestMapping se encarga de mapear una ruta URL
-    @GetMapping(value={"/","/index",""})
+    @GetMapping(value = {"/", "/index", ""})
     public String index(Model model) {
-        model.addAttribute("titulo","Hola Spring");
+        model.addAttribute("titulo", "Hola Spring");
         return "index";
     }
 
     @GetMapping("/perfil")
-    public String perfil(Model model){
-        model.addAttribute("titulo","Perfil del usuario - Spring");
-        model.addAttribute("usuario", new Usuario("Manuel","Ponton","manu@gmail.com"));
+    public String perfil(Model model) {
+        model.addAttribute("titulo", "Perfil del usuario - Spring");
+        model.addAttribute("usuario", new Usuario("Manuel", "Ponton", "manu@gmail.com"));
         return "perfil";
+    }
+
+    @GetMapping("/listar")
+    public String listar(Model model) {
+        model.addAttribute("titulo", "Lista de usuarios - Spring");
+        model.addAttribute("usuarios", Arrays.asList(
+                new Usuario("Carlos", "Lopez", ""),
+                new Usuario("Jorge", "Castro", "castro@gmail.com")));
+        return "listar";
     }
 }
